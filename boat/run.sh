c4bbf7728888e706f7936a6fc25a3aeee4a51503
trevordmeyer <35824308+trevordmeyer@users.noreply.github.com>
2018-09-26 17:00:52 +0000

    Update run.sh

--- boat/run.sh
+++ boat/run.sh
@@ -5,6 +5,7 @@ echo "Updating the GrovePi Firmware"
 echo
 
 sudo chmod +x /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
+cd /home/pi/Dexter/GrovePi/Firmware
 sudo /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
 
 echo "Seas the day."
