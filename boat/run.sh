9b0ce5cf95ecb2d6b85bb51bb3ed06fdb030cba6
trevordmeyer <35824308+trevordmeyer@users.noreply.github.com>
2018-09-26 16:51:54 +0000

    Add files via upload

--- boat/run.sh
+++ boat/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo
-echo "This file has not been configured yet!"
+echo "Updating the GrovePi Firmware"
 echo
+
+sudo chmod +x /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
+sudo /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
+
 echo "Seas the day."
 read temp
