ed3c9006321ddae9ea1f77613cc8fc919dc1f766
Nick Masso <nicholas.masso.14@gmail.com>
2019-01-11 00:06:05 -0500

    Update run.sh

--- donut/run.sh
+++ donut/run.sh
@@ -3,16 +3,21 @@
 echo
 echo "Running Donut update script"
 
+runv=0
 while true; do
-    read -p "Do you wish to run the account reset script? (y/n)" yn
+    read -p "Do you wish to run the account reset script? (y/n) " yn
     case $yn in
-        [Yy]* ) sudo /home/pi/Desktop/updates/donut/donut/engr16x_reset.sh; break;;
+        [Yy]* ) $runv=1; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-echo
+if [ $runv == 1 ];
+    sudo chmod +x /home/pi/Desktop/updates/donut/donut/engr16x_reset.sh
+    sudo /home/pi/Desktop/updates/donut/donut/engr16x_reset.sh;
+fi
+
 echo
 echo "--------------------------"
 echo "Donut has finished running"
