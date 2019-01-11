76885fd166724fce6defefe476996e4a48378916
Nick Masso <nicholas.masso.14@gmail.com>
2019-01-10 23:39:41 -0500

    Update run.sh

--- donut/run.sh
+++ donut/run.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 echo
-echo "This file has not been configured yet."
+echo "Running Donut update script"
+
+while true; do
+    read -p "Do you wish to run the account reset script? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo ./engr16x_reset.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 echo
-echo "Donut give up."
+echo
+echo "--------------------------"
+echo "Donut has finished running"
+echo
 read temp
