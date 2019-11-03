715aa207d48eecf4af2113c04cd8d6d77ab50410
Nick Masso <nicholas.masso.14@gmail.com>
2019-11-03 15:18:44 -0500

    now getting newest firmware
    old version did not merge from dexter's github.

--- boat/run.sh
+++ boat/run.sh
@@ -4,6 +4,13 @@ echo
 echo "Updating the GrovePi Firmware"
 echo
 
+cd /home/pi/Dexter/GrovePi/
+
+echo "Pulling newest version from Dexter's website"
+sudo git fetch origin
+sudo git reset --hard
+sudo git merge origin/master
+
 sudo chmod +x /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
 cd /home/pi/Dexter/GrovePi/Firmware
 sudo /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
