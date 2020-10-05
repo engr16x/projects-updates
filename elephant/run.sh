23a1e0e4aa0c52c0ab3c67591a0ae07a77a0dc40
David Li <davidli2881@gmail.com>
2020-10-04 23:27:00 -0400

    update script that sets wifi channelto 1, 6, or 11

--- elephant/run.sh
+++ elephant/run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 echo
-echo "This file has not been configured yet."
+echo "Updating network channels..."
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+
+random=$RANDOM
+channels=(1 6 11)
+
+let "randomIdx = random % 3"
+let "c = channels[randomIdx]"
+
+echo "Setting new channel to:" $c
 echo
+echo "Updated hostapd.conf:"
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+cd /etc/hostapd
+sudo sed 's/channel=.*/channel='$c'/g' hostapd.conf
+echo 
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+echo "Updated channel successfully! Press Ctrl-C to exist..."
+echo
+
 echo "Always keep your trunks on."
 read temp
