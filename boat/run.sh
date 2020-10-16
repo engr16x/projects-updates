0b79e5f6d7a91df8e8fce885ca6dc41cfc475cb1
starmaid <nicholas.masso.14@gmail.com>
2020-10-16 16:20:45 -0400

    updating boat to update grovepi library

--- boat/run.sh
+++ boat/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# updates the grovepi firmware
+# and updates the grovepi python library
 
 echo
 echo "Updating the GrovePi Firmware"
@@ -14,6 +16,25 @@ sudo git merge origin/master
 sudo chmod +x /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
 cd /home/pi/Dexter/GrovePi/Firmware
 sudo /home/pi/Dexter/GrovePi/Firmware/firmware_update.sh
+
+sudo apt-get install libffi-dev
+
+sudo chmod +x /home/pi/Dexter/GrovePi/Script/update_grovepi.sh
+su - pi -c "/home/pi/Dexter/GrovePi/Script/update_grovepi.sh"
+
+echo "copying new files to team account" 
+
+file="/etc/passwd"
+while IFS=: read -r f1 f2 f3 f4 f5 f6 f7
+do
+  if [[ $f1 == *"team"* ]]; 
+  then
+    #echo "$f1 found"
+    team=($f1)
+  fi
+done <"$file"
+
+cp -rvu /home/pi/Dexter /home/$team/Desktop/Dexter
 
 echo "Seas the day."
 read temp
