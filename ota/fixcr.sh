0bbbeb5e339a1bd6f13255de5da5505918673960
TrevortheGinger <tjmladner@yahoo.com>
2022-01-09 17:18:52 -0500

    finalized fixcr.sh script

--- ota/fixcr.sh
+++ ota/fixcr.sh
@@ -10,11 +10,11 @@ echo
 echo Correcting line endings
 echo
 
-sed s/bash\r/bash /home/pi/Desktop/updates/plane/plane/run.sh
-sed s/bash\r/bash /home/pi/Desktop/updates/boat/boat/run.sh
-sed s/bash\r/bash /home/pi/Desktop/updates/donut/donut/run.sh
-sed s/bash\r/bash /home/pi/Desktop/updates/elephant/elephant/run.sh
-sed s/bash\r/bash /home/pi/Desktop/updates/duck/duck/run.sh
+sudo sed -i 's/\r$//' /home/pi/Desktop/source_files/plane.sh
+sudo sed -i 's/\r$//' /home/pi/Desktop/source_files/boat.sh
+sudo sed -i 's/\r$//' /home/pi/Desktop/source_files/donut.sh
+sudo sed -i 's/\r$//' /home/pi/Desktop/source_files/elephant.sh
+sudo sed -i 's/\r$//' /home/pi/Desktop/source_files/duck.sh
 
 echo
 echo line endings have been updated
